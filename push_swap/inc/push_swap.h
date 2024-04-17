5e9c453f862413f29ed2f4ae58e3af783eee19a5
0h-laugh <yerotakomiro@gmail.com>
2024-04-17 16:20:02 +0200

    clarity added

--- push_swap/inc/push_swap.h
+++ push_swap/inc/push_swap.h
@@ -6,7 +6,7 @@
 /*   By: ojastrze <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 23:38:02 by ojastrze          #+#    #+#             */
-/*   Updated: 2024/04/17 15:39:19 by ojastrze         ###   ########.fr       */
+/*   Updated: 2024/04/17 16:18:22 by ojastrze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,51 +19,52 @@
 # include <stddef.h>
 # include <stdio.h>
 
+//My double linked list structure
 typedef struct s_node
 {
-	int				data;
-	int 			pos;
-	struct s_node	*next;
-	struct s_node	*prev;
+	int				data;		//value stored in node
+	int 			pos;		//position of node in stack
+	int 			cost;		//result of counting cost function
+	int 			median;		//median of all values
+	int				cheapest;	//here i store what has the lowest cost
+	struct s_node	*target;	//pointer to target node
+	struct s_node	*next;		//pointer to next node
+	struct s_node	*prev;		//pointer to previous node
 }		t_node;
 
-typedef struct s_stack
-{
-	int		size;
-	t_node	*top;
-	t_node	*bot;
-}		t_stack;
+//Errors
 
-//main funtions
-
-
-//check input
+//Check input
 int	check_input(char **av);
 
-//stack funtions
+//Stack init
 void	fill_stack(int ac, char **av, t_stack *stack_a);
 
-//stack utility
+//Node init
 void	create_node(t_stack *stack, int value);
 
-//stack operations
-//swaps
+//Stack utils
+
+//Commands
+	//Swaps
 void	swap_a(t_stack *stack_a);
 void	swap_b(t_stack *stack_b);
 void	swap_ab(t_stack *stack_a, t_stack *stack_b);
 
-//pushes
+	//Pushes
 void	push_a(t_stack *stack_a, t_stack *stack_b);
 void	push_b(t_stack *stack_a, t_stack *stack_b);
 
-//rotates
+	//Rotates
 void	rotate_a(t_stack *stack_a);
 void	rotate_b(t_stack *stack_b);
 void	rotate_ab(t_stack *stack_a, t_stack *stack_b);
 
-//reverse rotates
+	//Reverse rotates
 void	reverse_rotate_a(t_stack *stack_a);
 void	reverse_rotate_b(t_stack *stack_b);
 void	reverse_rotate_ab(t_stack *stack_a, t_stack *stack_b);
+
+//Algorithms
 
 #endif
