a86cff3187446cebfe8ac5fe698494b43cb6e7d6
0h-laugh <yerotakomiro@gmail.com>
2024-05-29 17:30:57 +0200

    check

--- push_swap/src/stack.c
+++ push_swap/src/stack.c
@@ -6,7 +6,7 @@
 /*   By: ojastrze <ojastrze@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 15:55:17 by ojastrze          #+#    #+#             */
-/*   Updated: 2024/05/29 15:58:28 by ojastrze         ###   ########.fr       */
+/*   Updated: 2024/05/29 17:00:52 by ojastrze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,22 @@
 
 void	init_stack(t_stack **stack, char **av)
 {
-//	for each value in input_values:
-//		if value is not valid:
-//			handle error
-//		if value is a duplicate in stack:
-//			handle error
-//		new_node = create new node with value
-//		if new_node creation failed:
-//			handle error
-//		add new_node to stack
+	int i;
+	int value;
+	t_stack *new_node;
+	i = 0;
+	while (av[i]) {
+		value = ft_atoi(av[i]);
+		new_node = create_new_node(value);
+		if (!new_node)
+			//error handling
+			return;
+		if (!check_input(char **av))
+		{
+			//error handling
+			return;
+		}
+		i++;
+	}
+	add_new_node(**stack)
 }
