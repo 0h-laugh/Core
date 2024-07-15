cdefaf887ec6a891d3c2e45a9981559331be9ed5
0h-laugh <yerotakomiro@gmail.com>
2024-07-15 17:42:06 +0200

    cleaning

--- minitalk/src/server.c
+++ minitalk/src/server.c
@@ -6,13 +6,24 @@
 /*   By: ojastrze <ojastrze@stundent.42warsaw.pl>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 08:40:44 by ojastrze          #+#    #+#             */
-/*   Updated: 2024/07/15 12:34:46 by ojastrze         ###   ########.fr       */
+/*   Updated: 2024/07/15 16:08:28 by ojastrze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minitalk.h"
 
-void	catch_signal(int signal, siginfo_t *info, void *context)
+void	catch_signal(int signal)
 {
+	static int	bit;
+	static int	i;
 
+	if (signal == SIGUSR1)
+		i |= (0x01 << bit);
+	bit++;
+	if (bit == 8)
+	{
+		ft_printf("%c", i);
+		bit = 0;
+		i = 0;
+	}
 }
