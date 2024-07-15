bd8c2b13f27bd6ae8809f83928a94de31d97316e
0h-laugh <yerotakomiro@gmail.com>
2024-07-15 13:46:16 +0200

    fix

--- push_swap/src/sort.c
+++ push_swap/src/sort.c
@@ -6,7 +6,7 @@
 /*   By: ojastrze <ojastrze@stundent.42warsaw.pl>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 18:29:19 by ojastrze          #+#    #+#             */
-/*   Updated: 2024/07/09 13:02:55 by ojastrze         ###   ########.fr       */
+/*   Updated: 2024/07/09 13:15:08 by ojastrze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,11 +33,11 @@ void	ft_sort_int_tab(int *tab, int size)
 	}
 }
 
-int	get_pivot(t_stack *stack, int size)
+int get_pivot(t_stack *stack, int size)
 {
-	int	*pivot_array;
-	int	i;
-	t_stack	*temp;
+	int *pivot_array;
+	int i;
+	t_stack *temp;
 
 	pivot_array = (int *)malloc(sizeof(int) * size);
 	if (!pivot_array)
@@ -55,10 +55,10 @@ int	get_pivot(t_stack *stack, int size)
 	return (i);
 }
 
-void	partition(t_stack **stack_a, t_stack **stack_b, int pivot, int size)
+void partition(t_stack **stack_a, t_stack **stack_b, int pivot, int size)
 {
-	int	i;
-	int	pushed;
+	int i;
+	int pushed;
 
 	i = 0;
 	pushed = 0;
@@ -77,23 +77,26 @@ void	partition(t_stack **stack_a, t_stack **stack_b, int pivot, int size)
 		reverse_rotate(stack_b);
 }
 
-void	quick_sort_util(t_stack **stack_a, t_stack **stack_b, int size)
+void quick_sort_util(t_stack **stack_a, t_stack **stack_b, int size)
 {
-	int	pivot;
+	int pivot;
 
 	if (size < 2)
 		return;
 	pivot = get_pivot(*stack_a, size);
 	partition(stack_a, stack_b, pivot, size);
+
+	// Recursively sort both parts
 	quick_sort_util(stack_a, stack_b, size / 2);
-	quick_sort_util(stack_b, stack_a, size / 2);
+	quick_sort_util(stack_b, stack_a, size - (size / 2)); // corrected to sort remaining elements
+
 	while (*stack_b)
 		push(stack_b, stack_a);
 }
 
-void	quick_sort(t_stack **stack_a, t_stack **stack_b)
+void quick_sort(t_stack **stack_a, t_stack **stack_b)
 {
-	int	size;
+	int size;
 
 	size = ft_lstsize(*stack_a);
 	quick_sort_util(stack_a, stack_b, size);
