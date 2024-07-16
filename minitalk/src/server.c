e76106e5e5ffc5e3dfe1e97450413534719e39bc
0h-laugh <yerotakomiro@gmail.com>
2024-07-16 14:03:55 +0200

    added ft_catch

--- minitalk/src/server.c
+++ minitalk/src/server.c
@@ -6,19 +6,19 @@
 /*   By: ojastrze <ojastrze@stundent.42warsaw.pl>   +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/27 08:40:44 by ojastrze          #+#    #+#             */
-/*   Updated: 2024/07/15 16:08:28 by ojastrze         ###   ########.fr       */
+/*   Updated: 2024/07/16 12:59:27 by ojastrze         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../inc/minitalk.h"
 
-void	catch_signal(int signal)
+void	ft_catch(int signal)
 {
 	static int	bit;
 	static int	i;
 
 	if (signal == SIGUSR1)
-		i |= (0x01 << bit);
+		i |= (1 << bit);
 	bit++;
 	if (bit == 8)
 	{
@@ -26,4 +26,27 @@ void	catch_signal(int signal)
 		bit = 0;
 		i = 0;
 	}
+}
+
+int	main(int ac, char **av)
+{
+	int pid;
+
+	(void)av;
+	if (ac != 1)
+	{
+		ft_printf("Error: Bad usage!\n");
+		ft_printf("Type: ./server\n");
+		return (0);
+	}
+	pid = getpid();
+	ft_printf("PID:%d\n", pid);
+	ft_printf("Waiting for a message...\n");
+	while (ac == 1)
+	{
+		signal(SIGUSR1, ft_catch);
+		signal(SIGUSR2,	ft_catch);
+		pause ();
+	}
+	return (0);
 }
